50d2083234015f7fc927258aaed90d70d5e174d8
Catalin Stanciu <catacsdev@gmail.com>
2016-10-30 19:48:26 +0100

    Add handlers for details edition

--- src/reducers/reducer.ts
+++ src/reducers/reducer.ts
@@ -9,15 +9,38 @@ export const initialState = [
 // Definimos tipos de actions
 export const ADD_LIST = 'ADD_LIST';
 export const DELETE_LIST = 'DELETE_LIST';
+export const ADD_LIST_ITEM = 'ADD_LIST_ITEM';
+export const DELETE_LIST_ITEM = 'DELETE_LIST_ITEM';
+
+function addList(state, list) {
+  return [...state, list];
+}
+
+function deleteList(state, id) {
+  return state.filter(list => list.id !== id);
+}
+
+function addListItem(state, list) {
+  // TODO Añadir item
+  return state;
+}
+
+function deleteListItem(state, list) {
+  // TODO Borrar item
+  return state;
+}
 
 // 13/ Creamos reducer, pasando el estado inicial y un parámetro action
 export const reducer = (state = initialState, action: Action) => {
   switch (action.type) {
 		case ADD_LIST:
-			const list = action.payload;
-      return [...state, list];
+      return addList(state, action.payload);
 		case DELETE_LIST:
-			return state.filter(list => list.id !== action.payload.id);
+			return deleteList(state, action.payload.id);
+    case ADD_LIST_ITEM:
+      return addListItem(state, action.payload);
+    case DELETE_LIST_ITEM:
+    return deleteListItem(state, action.payload);
     default:
       return state;
   }
